29c497a72e187e80d8955a8ffef9f961a4b544b1
Antoine Barberin <antoine.barberin@student.isae-supaero.fr>
2024-06-20 16:50:10 +0200

    Structure de la MenuView réalisée mais il faut la peaufiner

--- TajMahal/Model.swift
+++ TajMahal/Model.swift
@@ -15,7 +15,8 @@ enum SpiceLevel {
 }
 
 // Représente l'objet "plat", qui figure sur la carte du menu
-struct Dish {
+struct Dish: Identifiable {
+    var id: Int
     var name: String
     var description: String
     var allergens: String
--- TajMahal/Views/MenuView.swift
+++ TajMahal/Views/MenuView.swift
@@ -13,12 +13,65 @@ struct MenuView: View {
     let viewModel: ViewModel = ViewModel()
     
     var body: some View {
-        List {
-            // À completer
+        VStack{
+            Text("Menu")
+                .font(.title)
+            VStack (alignment: .leading){
+                Text("Entrées")
+                    .padding(.leading, 20)
+                    .font(.title)
+                List(viewModel.apetizerArray) { dish in
+                    LineDishView(dish: dish)
+                }
+                .padding()
+                Text("Plats principaux")
+                    .padding(.leading, 20)
+                    .font(.title)
+                List(viewModel.mainCourseArray) { dish in
+                    LineDishView(dish: dish)
+                }
+            }
         }
     }
 }
 
+
+//Structure d'une ligne du menu
+
+struct LineDishView: View {
+    var dish: Dish
+    
+    var body: some View{
+        ZStack{
+            RoundedRectangle(cornerRadius: 10)
+                .frame(width: 335, height: 110)
+                .foregroundColor(.white)
+            HStack{
+                Image(dish.imageName)
+                    .resizable()
+                    .frame(width: 128, height: 86)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                    .aspectRatio(contentMode: .fill)
+                Spacer()
+                VStack(alignment: .leading, spacing: 10){
+                    Text(dish.name)
+                        .font(.headline)
+                    Text(dish.description)
+                        .font(.system(size: 12))
+                        .foregroundStyle(.gray)
+                    HStack{
+                        Text("Price")
+                            .font(.subheadline)
+                        Spacer()
+                        Text("Spice level")
+                            .font(.subheadline)
+                    }
+                }
+            }
+            .padding()
+        }
+    }
+}
 #Preview {
     MenuView()
 }
